4443b9e12c2827fc338a7c517e5469bae1c07dc2
fmiodek <fmiodek@gmx.de>
2024-07-01 14:10:25 +0200

    comm

--- start_balloons.sh
+++ start_balloons.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sleep 20
-./connect.py &
-./frontend_comm.py &
-./frontend_server.py &
+python3 connect.py &
+python3 frontend_comm.py &
+python3 frontend_server.py &
 "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --kiosk "http://localhost:8000/scores.html" &
 
